4abe32da47f86770fc73e8c73ef8a7e98ded1242
mahdi.hachicha <mahdi.hachicha@stud.enis.tn>
2021-05-25 08:53:40 +0100

    Updates

---
+++ precache-manifest.f6d91ea7e39845bcbf0206a674f321ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "369ed67744119279b4610de0da03a145",
+    "url": "/msApp/index.html"
+  },
+  {
+    "revision": "5ce8e02f00790670908b",
+    "url": "/msApp/static/js/2.a9f28d76.chunk.js"
+  },
+  {
+    "revision": "7f9f1b1edb468283376722fb76c9e8a2",
+    "url": "/msApp/static/js/2.a9f28d76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bef21564f1058cadc898",
+    "url": "/msApp/static/js/main.d677bbe6.chunk.js"
+  },
+  {
+    "revision": "b0dae15b6cf6234a67b5",
+    "url": "/msApp/static/js/runtime-main.19d24a4b.js"
+  }
+]);
